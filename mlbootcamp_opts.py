9c42b131de7b29e5989287f387d75f72ba672f5d
Alexey Ponkin <aponkin@luxoft.com>
2017-03-12 19:59:19 +0300

    hyperopts calculate

--- mlbootcamp_opts.py
+++ mlbootcamp_opts.py
@@ -7,6 +7,7 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import Imputer, StandardScaler, MinMaxScaler
 from sklearn.svm import SVC
 from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier
 
 import numpy as np
 import pandas as pd
@@ -14,24 +15,41 @@ import xgboost as xgb
 
 X_train = pd.read_csv('files/x_train.csv', sep=';').drop(['doReturnOnLowerLevels'], axis=1)
 y_train = pd.read_csv('files/y_train.csv', sep=';', header=None)
-pipe = make_pipeline(StandardScaler(), PCA(n_components=8, whiten=True))
+X_train['averageLevelsPerDay'] = X_train['numberOfAttemptedLevels'] / X_train['numberOfDaysActuallyPlayed']
+X_train['averageAttemptsPerLevel'] = (X_train['totalNumOfAttempts'] - X_train['attemptsOnTheHighestLevel']) / (X_train['numberOfAttemptedLevels'] - 1)
+X_train['avarageScorePerLevel'] = X_train['totalScore'] / X_train['numberOfAttemptedLevels']
+X_train['wastedBoosters'] = X_train['fractionOfUsefullBoosters'] * X_train['numberOfBoostersUsed']
+X_train['userProgress'] = X_train['numberOfAttemptedLevels'] / ((X_train['maxPlayerLevel'] + 1)*X_train['numberOfDaysActuallyPlayed'])
+X_train['attemptsRatio'] = X_train['attemptsOnTheHighestLevel'] / X_train['totalNumOfAttempts']
+X_train['stuckOnHighest'] = X_train['attemptsOnTheHighestLevel'] / X_train['averageAttemptsPerLevel']
+X_train['minPlayerLevel'] = X_train['maxPlayerLevel']-((X_train['totalNumOfAttempts'] - X_train['attemptsOnTheHighestLevel']))
+X_train['playerLevelDelta'] = X_train['maxPlayerLevel'] - X_train['minPlayerLevel']
+X_train['avarageScorePerAttempt'] = X_train['avarageScorePerLevel']/X_train['averageAttemptsPerLevel']
+X_train = X_train.fillna(0)
+pipe = make_pipeline(StandardScaler())
 X_train_trans = pipe.fit_transform(X_train)
 y_train_trans = y_train[0].values
-X_train1, X_test1, y_train1, y_test1 = train_test_split(X_train_trans, y_train_trans, test_size=0.5, random_state=17)
 
 space = {
-    'max_depth': hp.choice('max_depth', np.arange(3, 10, dtype=int)),
+    'max_depth': hp.choice('max_depth', np.arange(0, 10, dtype=int)),
     'min_child_weight': hp.quniform('x_min_child', 1, 10, 1),
-    'subsample': hp.uniform('x_subsample', 0.7, 1),
-    'n_estimators': hp.choice('x_n_estimators', np.arange(800, 10000, dtype=int)),
-    'learning_rate': hp.uniform('x_learning_rate', 0.0001, 0.3)
+    'subsample': hp.uniform('x_subsample', 0.6, 1),
+    'n_estimators': hp.choice('x_n_estimators', np.arange(10, 300, dtype=int)),
+    'learning_rate': hp.uniform('x_learning_rate', 0.0001, 0.9)
 }
 
-spaceSvm = {
-        'C': hp.quniform('C', 1, 10, 1),
-        'gamma': hp.uniform('gamma', 0.01, 0.0001),
-        'kernel': hp.choice('kernel', ['linear', 'rbf'])
-        }
+space4svm = {
+    'C': hp.uniform('C', 0, 20),
+    'kernel': hp.choice('kernel', ['linear', 'sigmoid', 'poly', 'rbf']),
+    'gamma': hp.uniform('gamma', 0, 20),
+}
+
+space4rf = {
+    'max_depth': hp.choice('max_depth', range(1,20)),
+    'max_features': hp.choice('max_features', range(1,15)),
+    'n_estimators': hp.choice('n_estimators', range(1,20)),
+    'criterion': hp.choice('criterion', ["gini", "entropy"]),
+}
 
 def objSVM(s):
     clf = SVC(
@@ -47,6 +65,14 @@ def objSVM(s):
     print("SCORE: %0.7f" % loss)
     return {'loss': loss, 'status': STATUS_OK}
 
+def objRf(params):
+    clf = RandomForestClassifier(**params)
+    kfold = StratifiedKFold(n_splits=5, shuffle=True)
+    score = cross_val_score(clf, X_train_trans, y_train_trans, scoring='neg_log_loss', cv=kfold)
+    loss = abs(score.mean())
+    print("SCORE: %0.7f" % loss)
+    return {'loss': loss, 'status': STATUS_OK}
+
 def objective(space):
 
     clf = xgb.XGBClassifier(
@@ -55,23 +81,15 @@ def objective(space):
         max_depth=space['max_depth'],
         min_child_weight=space['min_child_weight'],
         subsample=space['subsample'])
+    kfold = StratifiedKFold(n_splits=5, shuffle=True)
+    score = cross_val_score(clf, X_train_trans, y_train_trans, scoring='neg_log_loss', cv=kfold)
 
-    eval_set = [(X_train1, y_train1), (X_test1, y_test1)]
-
-    clf.fit(
-        X_train1,
-        y_train1,
-        eval_set=eval_set,
-        eval_metric="logloss",
-        early_stopping_rounds=50)
-
-    pred = clf.predict_proba(X_test1)[:, 1]
-    auc = abs(log_loss(y_test1, pred))
+    auc = abs(score.mean())
     print("SCORE: %0.7f" % auc)
     return {'loss': auc, 'status': STATUS_OK}
 
 
 if __name__ == "__main__":
     trials = Trials()
-    best = fmin( fn=objective, space=space, algo=tpe.suggest, max_evals=25, trials=trials)
+    best = fmin( fn=objective, space=space, algo=tpe.suggest, max_evals=300, trials=trials)
     print(best)
